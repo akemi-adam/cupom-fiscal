c982f5d661864373f21c737e88db8d207ceee723
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-10-21 09:19:53 -0300

    - Criei um novo layout chamado pages (Ele é o padrão para todas as páginas, mas o dashboard não usa ele
    - Falta adicionar as rotas de algumas páginas

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -77,7 +77,7 @@
                 </ol>
             </div>
             </nav>
-        </div><!-- End Breadcrumbs -->
+        </div>
 
         <section class="sample-page">
             <div class="container" data-aos="fade-up">
@@ -100,45 +100,29 @@
                 <a href="index.html" class="logo d-flex align-items-center">
                 <span>Hugo Market</span>
                 </a>
-                <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-                <div class="social-links d-flex mt-4">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                </div>
+                <p>O sistema mais confiável para gerenciar seu estabelecimento</p>
             </div>
 
             <div class="col-lg-2 col-6 footer-links">
                 <h4>Useful Links</h4>
                 <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Terms of service</a></li>
-                <li><a href="#">Privacy policy</a></li>
-                </ul>
-            </div>
-
-            <div class="col-lg-2 col-6 footer-links">
-                <h4>Our Services</h4>
-                <ul>
-                <li><a href="#">Web Design</a></li>
-                <li><a href="#">Web Development</a></li>
-                <li><a href="#">Product Management</a></li>
-                <li><a href="#">Marketing</a></li>
-                <li><a href="#">Graphic Design</a></li>
+                <li><a href="/">Dashboard</a></li>
+                <li><a href="#">Cadastrar Empresas</a></li>
+                <li><a href="#">Ver Empresas</a></li>
+                <li><a href="#">Cadastrar Instituições</a></li>
+                <li><a href="#">Cadastrar Nota Fiscal</a></li>
+                <li><a href="#">Fazer Doação</a></li>
                 </ul>
             </div>
 
             <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-                <h4>Contact Us</h4>
+                <h4>Contate-nos</h4>
                 <p>
-                A108 Adam Street <br>
-                New York, NY 535022<br>
-                United States <br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
+                Pereirão<br>
+                Caicó, RN, 59300-000<br>
+                Brasil <br><br>
+                <strong>Telefone:</strong> 84 8653-0701<br>
+                <strong>Email:</strong> ultragas@gmail.com<br>
                 </p>
 
             </div>
@@ -148,7 +132,7 @@
 
         <div class="container mt-4">
             <div class="copyright">
-            &copy; Copyright <strong><span>Impact</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>HugoMarket</span></strong>. All Rights Reserved
             </div>
         </div>
 
