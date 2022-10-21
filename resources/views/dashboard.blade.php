373fff5304106908fa8815b9da7e422dd8aa5b33
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-10-20 22:30:19 -0300

    Dashboard finalizado, layout modificado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,11 +3,74 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <ul class="mt-4">
-        <li><a href="{{ route('enterprise.create') }}">Cadastrar empresa</a></li>
-        <li><a href="{{ route('enterprise.index') }}">Ver empresas</a></li>
-        <li><a href="{{ route('institution.create') }}">Cadastrar instituição</a></li>
-        <li><a href="">Cadastrar nota fiscal</a></li>
-        <li><a href="">Fazer doação</a></li>
-    </ul>
+<header id="header" class="header d-flex align-items-center">
+
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="/" class="logo d-flex align-items-center">
+        <h1>Hugo<span>Market</span></h1>
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="{{ route('enterprise.create') }}">Cadastrar Empresa</a></li>
+          <li><a href="{{ route('enterprise.index') }}">Ver Empresas</a></li>
+          <li><a href="{{ route('institution.create') }}">Cadastrar Instituições</a></li>
+          <li><a href="#">Cadastrar Nota Fiscal</a></li>
+          <li><a href="#">Fazer Doação</a></li>
+        </ul>
+      </nav>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+    </div>
+</header>
+  <section id="hero" class="hero">
+    <div class="container position-relative">
+      <div class="row gy-5" data-aos="fade-in">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
+          <h2>Bem-Vindo ao <span>Hugo Market</span></h2>
+          <p>Melhor sistema para gerenciamento de mercados e cupons</p>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2">
+          <img src="assets/img/hero-img.svg" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
+        </div>
+      </div>
+    </div>
+
+    <div class="icon-boxes position-relative">
+      <div class="container position-relative">
+        <div class="row gy-4 mt-5">
+
+          <div class="col-xl-3 col-md-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-inboxes-fill"></i></div>
+              <h4 class="title"><a href="{{ route('enterprise.create') }}"" class="stretched-link">Cadastrar Empresa</a></h4>
+            </div>
+          </div>
+          <!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-building"></i></div>
+              <h4 class="title"><a href="{{ route('institution.create') }}" class="stretched-link">Cadastrar Instituições</a></h4>
+            </div>
+          </div>
+          <!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-card-text"></i></div>
+              <h4 class="title"><a href="" class="stretched-link">Cadastrar Nota Fiscal</a></h4>
+            </div>
+          </div>
+          <!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-4" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-cash-coin"></i></div>
+              <h4 class="title"><a href="" class="stretched-link">Fazer Doação</a></h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,23 @@
     <!-- Bootstrap -->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="{{asset('Impact/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{asset('Impact/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('Impact/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('Impact/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('Impact/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('Impact/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('Impact/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  
+    <!-- Template Main CSS File -->
+    <link href="{{asset('Impact/assets/css/main.css')}}" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" defer></script>
 
@@ -19,7 +36,7 @@
 </head>
 <body>
 
-    <div class="container">
+    <div>
 
         @if (session('message'))
             <div class="alert alert-success mt-4">
@@ -33,5 +50,15 @@
 
     @livewireScripts
 
+    <script src="{{asset('Impact/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/aos/aos.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('Impact/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('Impact/assets/js/main.js')}}"></script>
 </body>
 </html>
